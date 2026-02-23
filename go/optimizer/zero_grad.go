fbf0f495bfbbad6767b5f5da2d2af409cc887e6a
Olabode Adedoyin <olabode.eesti@gmail.com>
2026-02-23 06:49:07 +0200

    Fixed issue with optimizer creating new tensors and exploding memory

--- go/optimizer/zero_grad.go
+++ go/optimizer/zero_grad.go
@@ -1,11 +1,31 @@
 package optimizer
 
+/*
+#cgo CFLAGS: -I../../base
+#cgo LDFLAGS: -L../../base/build -L../../base/OpenBLAS/install/lib -lshapes_core -lshapes_memory -lopenblas -lm
+
+#include "tensor/tensor.h"
+#include "common.h"
+#include <string.h>
+
+static inline void zeroTensorValues(Tensor *t) {
+	if (t != NULL && t->values != NULL) {
+		size_t bytes = t->size * getBytesForDtype(t->dtype);
+		memset(t->values, 0, bytes);
+	}
+}
+*/
+import "C"
+
 import (
 	shapes "github.com/flygerian/shapes"
 )
 
 func ZeroGrad(ctx *shapes.Context, cg *shapes.ComputationGraph) {
 	for _, node := range cg.Nodes {
-		node.Grad = shapes.Zeros(ctx, shapes.ShapeOf(node.Grad))
+		// Instead of allocating a new zero tensor, just zero out the existing gradient's values
+		if node.Grad != nil {
+			C.zeroTensorValues((*C.Tensor)(node.Grad.UnsafeCTensor()))
+		}
 	}
 }
