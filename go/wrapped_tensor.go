da7082448c5442de91210475f2f8689888b6a23f
Olabode Adedoyin <olabode.eesti@gmail.com>
2026-05-30 13:20:21 +0300

    improving the gitignore and removing go files

--- go/wrapped_tensor.go
+++
@@ -1,8 +0,0 @@
-package shapes
-
-// WrappedTensor wraps a Tensor and its Context to provide a fluent API
-// where operations don't require passing the context as the first parameter.
-type WrappedTensor struct {
-	tensor  Tensor
-	context Context
-}
